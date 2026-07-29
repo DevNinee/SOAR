5942a038643e80107192f61e6207646878eefd36
NANOSec <aeliton.me@gmail.com>
2026-07-29 14:00:17 -0300

    Implement user confirmation for network scanning
    Added a function to confirm network scanning initiation with user input.

--- soar-network-monitor/src/main.py
+++ soar-network-monitor/src/main.py
@@ -1,1 +1,30 @@
+#definir uma rede
+
+def solicitar_confirmação(rede):
+  """
+  Mostrar a rede selecionada e perguntar se o usuario deseja continuar """
+print("=" * 50)
+print("SOAR DE DISPOSITIVOS DE REDE LOCAL")
+print("=" * 50)
+print(f"Rede selecionada: {rede}")
+
+resposta = input("deseja iniciar o escaneamento? [y/n]")
+if resposta == "y"
+  print("Scanner autorizado! iniciando...")
+     return True
+  print("escaneamento cancelado!")
+     return False 
+
+def main()
+'''função principal do programa'''
+
+rede_alvo = 192.168.1.0/24
+autorizada = solicitar_confirmação(rede)
+ if autorizada:
+   print(f"iniciando a analise da rede!{rede}...")
+ else:
+   print("O programa sera encerrado!")
+
+if name == "__main__":
+  main()
 
